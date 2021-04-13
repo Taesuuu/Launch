f91f79f5b8d30a43316d77329f88e10265be5dac
naseokhun <81973857+naseokhun@users.noreply.github.com>
2021-04-13 23:01:47 +0900

    난이도
    아 개빡치네

---
+++ Assets/Script/ach.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ach : MonoBehaviour
+{
+    public Button[] lebtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int lea = PlayerPrefs.GetInt("lea", 2);
+
+        for(int i = 0; i < lebtn.Length; i++)
+        {
+            if (i + 2 > lea)
+                lebtn[i].interactable = false;
+        }
+    }
+
+    
+}
