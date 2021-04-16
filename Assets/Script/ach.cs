5f142976cb4def205a26b9237408ca47dc555d80
naseokhun <81973857+naseokhun@users.noreply.github.com>
2021-04-16 22:19:41 +0900

    ???
    ????

--- Assets/Script/ach.cs
+++ Assets/Script/ach.cs
@@ -6,18 +6,20 @@ using UnityEngine.UI;
 public class ach : MonoBehaviour
 {
     public Button[] lebtn;
+    private int counter;
+
     // Start is called before the first frame update
     void Start()
     {
-  
+
         int lea = PlayerPrefs.GetInt("lea", 2);
 
-        for(int i = 0; i < lebtn.Length; i++)
+        for (int i = 0; i < lebtn.Length; i++)
         {
             if (i + 2 > lea)
                 lebtn[i].interactable = false;
+
+           
         }
     }
-
-    
 }
