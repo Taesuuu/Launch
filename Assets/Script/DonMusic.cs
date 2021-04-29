5d3282975a201aead20dfdd93cae659726c4ddda
naseokhun <81973857+naseokhun@users.noreply.github.com>
2021-04-29 16:34:49 +0900

    ?
    ?

--- Assets/Script/DonMusic.cs
+++ Assets/Script/DonMusic.cs
@@ -7,33 +7,7 @@ public class DonMusic : MonoBehaviour
 
     private void Start()
     {
-        // DontDestroyOnLoad(gameObject);
-        song();
-       
+       DontDestroyOnLoad(gameObject);    
     }
 
-    // Update is called once per frame
-   void song()
-    {
-        if (Play1.currentSong1 == 0)
-        {
-            AudioManager.instance.PlayBGM("BGM" + 0);
-        }
-        if (Play1.currentSong1 == 1)
-        {
-            AudioManager.instance.PlayBGM("BGM" + 1);
-        }
-        if (Play1.currentSong1 == 2)
-        {
-            AudioManager.instance.PlayBGM("BGM" + 2);
-        }
-        if (Play1.currentSong1 == 3)
-        {
-            AudioManager.instance.PlayBGM("BGM" + 3);
-        }
-        if (Play1.currentSong1 == 4)
-        {
-            AudioManager.instance.PlayBGM("BGM" + 4);
-        }
-    }
 }
